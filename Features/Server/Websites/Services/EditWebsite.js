34cdec77f90a60f610902f6861e8c6ad2728ba31
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-06-06 12:57:37 +0300

    change

--- Features/Server/Websites/Services/EditWebsite.js
+++ Features/Server/Websites/Services/EditWebsite.js
@@ -3,20 +3,23 @@ const asyncHandeler = require('express-async-handler');
 const Website = require('../../../../Models/Website');
 
 exports.editWebsite = asyncHandeler(async (req, res, next) => {
+    
+    
     const { name, available, userId } = req.body;
     let parsedUrl = (new URL(`${req.body.url}`)).origin;
+  
     try {
         const website = await Website.findOneAndUpdate({
-            url: parsedUrl,
+            _id: req.params.id,
             userId: userId,
         }, {
             name,
             url: parsedUrl,
             available,
         }, { new: true });
+        console.log(website);
         return res.status(200).json({ website });
     } catch (ex) {
-
         return res.sendStatus(500);
     }
 });
